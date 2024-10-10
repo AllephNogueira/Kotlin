9bcbb4aee394541b7bd9a12f041ad9c30cd9c88a
Alleph <allephn@hotmail.com.br>
2024-10-10 11:23:39 -0300

    ScrollView - Rolagem de tela

--- Curso-Android-Kotlin/Construcao-de-layout/app/src/main/java/com/allephnogueira/construcaolayout/ScrollView.java
+++ Curso-Android-Kotlin/Construcao-de-layout/app/src/main/java/com/allephnogueira/construcaolayout/ScrollView.java
@@ -1,8 +1,6 @@
 package com.allephnogueira.construcaolayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PaddingEMargem extends AppCompatActivity {
-
+public class ScrollView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_padding_emargem);
+        setContentView(R.layout.activity_scroll_view);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void entendedoXML (View view) {
-        Intent intent = new Intent(this, XML.class);
-        startActivity(intent);
-    }
-
-
-    public void acessandoOScrollView(View view) {
-        Intent intent = new Intent(this, ScrollView.class);
-        startActivity(intent);
     }
 }
