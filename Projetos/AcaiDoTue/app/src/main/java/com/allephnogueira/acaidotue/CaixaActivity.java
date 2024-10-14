4066d54c1fa6ca229edb2fa041883305e3473216
Alleph <allephn@hotmail.com.br>
2024-10-14 16:20:59 -0300

    Evento de click

--- Projetos/AcaiDoTue/app/src/main/java/com/allephnogueira/acaidotue/CaixaActivity.java
+++ Projetos/AcaiDoTue/app/src/main/java/com/allephnogueira/acaidotue/CaixaActivity.java
@@ -1,6 +1,8 @@
 package com.allephnogueira.acaidotue;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,29 @@ public class CaixaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Podemos criar no onCreate
+        // onCreate = quando a tela for criada.
+        // Então aqui vamos fazer tudo quando a tela estiver sendo criada, vai ser criado tudo de forma automatica.
+
+
+        // Vamos pegar a referencia do botao para fazer o evento de click
+
+        Button botao = findViewById(R.id.botaoSalvar); // Aqui vamos retornar um objeto ai podemos manipular da forma que quiser.
+        // setOnClickListener configura açao para o botao quando for clicada
+        botao.setOnClickListener(new View.OnClickListener() {
+            //onClickListener é onde vamos colocar as ações que queremos fazer.
+            @Override
+            public void onClick(View view) {
+                // Vamos por o codigo a ser executado
+                System.out.println("Botão foi clicado.");
+            }
+        }); // Dentro do parametro que vamos colocar a ação que queremos executar
+
+
+
     }
+
+
 }
