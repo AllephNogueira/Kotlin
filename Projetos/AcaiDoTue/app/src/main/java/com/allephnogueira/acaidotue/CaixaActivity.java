c4e8f12daf45578ee7934a73feb98ff727dce64f
Alleph <allephn@hotmail.com.br>
2024-10-14 16:34:29 -0300

    Recuperando valores de um EditText

--- Projetos/AcaiDoTue/app/src/main/java/com/allephnogueira/acaidotue/CaixaActivity.java
+++ Projetos/AcaiDoTue/app/src/main/java/com/allephnogueira/acaidotue/CaixaActivity.java
@@ -3,12 +3,15 @@ package com.allephnogueira.acaidotue;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CaixaActivity extends AppCompatActivity {
 
@@ -24,23 +27,25 @@ public class CaixaActivity extends AppCompatActivity {
         });
 
 
-        // Podemos criar no onCreate
-        // onCreate = quando a tela for criada.
-        // Então aqui vamos fazer tudo quando a tela estiver sendo criada, vai ser criado tudo de forma automatica.
+        Button botao = findViewById(R.id.botaoSalvar);
 
 
-        // Vamos pegar a referencia do botao para fazer o evento de click
+        TextInputEditText editText_email = findViewById(R.id.edit_email); // Pegando a referencia do objeto.
 
-        Button botao = findViewById(R.id.botaoSalvar); // Aqui vamos retornar um objeto ai podemos manipular da forma que quiser.
-        // setOnClickListener configura açao para o botao quando for clicada
+
+
         botao.setOnClickListener(new View.OnClickListener() {
-            //onClickListener é onde vamos colocar as ações que queremos fazer.
             @Override
             public void onClick(View view) {
-                // Vamos por o codigo a ser executado
-                System.out.println("Botão foi clicado.");
+
+                //1 Pegamos o objeto
+                //2 Pegamos o texto que esta dentro do objeto
+                //3 Convertemos para uma string
+                String emailUsuario = editText_email.getText().toString();
+
+                System.out.println("Botão foi clicado " + emailUsuario);
             }
-        }); // Dentro do parametro que vamos colocar a ação que queremos executar
+        });
 
 
 
