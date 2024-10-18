63773ea085396ebba9dc399561302ba07cc9e282
Alleph <allephn@hotmail.com.br>
2024-10-18 16:21:22 -0300

    Protected

---
+++ Curso-Android-Kotlin/Kotlin/Kotlin/src/POO/Classes/TesteEncapsulamento.kt
@@ -0,0 +1,17 @@
+package POO.Classes
+
+fun main() {
+//    val carro = Carro()
+//    carro.modelo = "C4"
+//    carro.acelerar()
+
+
+
+    val c4 = C4()
+    c4.acelerar()
+
+
+    val gol = Gol()
+    gol.acelerar()
+
+}
