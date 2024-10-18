08e389dc37210f64d69fc0f840ee421ce7dd0e7a
Alleph <allephn@hotmail.com.br>
2024-10-18 18:20:30 -0300

    Internal

--- Curso-Android-Kotlin/Kotlin/Kotlin/src/POO/Classes/Encapsulamento.kt
+++ Curso-Android-Kotlin/Kotlin/Kotlin/src/POO/Classes/Encapsulamento.kt
@@ -1,35 +1,35 @@
-package POO.Classes
-
-open class Carro {
-    var modelo = ""
-
-
-    protected open fun quantidadeDeCombustivel(){
-        println("Adicionando combustivel")
-    }
-
-    fun acelerar(){
-        quantidadeDeCombustivel()
-        println("Acelerando o carro")
-    }
-}
-
-
-class C4 : Carro() {
-
-    override fun quantidadeDeCombustivel(){
-        println("Adicionando 10 ml de combustivel")
-    }
-}
-
-class Gol: Carro() {
-    override fun quantidadeDeCombustivel(){
-        println("Adicionando 5ml de combustivel")
-    }
-
-}
-
-fun main() {
-    val carro = Carro()
-    carro.acelerar()
-}
+//package POO.Classes
+//
+//open class Carro {
+//    var modelo = ""
+//
+//
+//    protected open fun quantidadeDeCombustivel(){
+//        println("Adicionando combustivel")
+//    }
+//
+//    fun acelerar(){
+//        quantidadeDeCombustivel()
+//        println("Acelerando o carro")
+//    }
+//}
+//
+//
+//class C4 : Carro() {
+//
+//    override fun quantidadeDeCombustivel(){
+//        println("Adicionando 10 ml de combustivel")
+//    }
+//}
+//
+//class Gol: Carro() {
+//    override fun quantidadeDeCombustivel(){
+//        println("Adicionando 5ml de combustivel")
+//    }
+//
+//}
+//
+//fun main() {
+//    val carro = Carro()
+//    carro.acelerar()
+//}
