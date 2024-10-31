ff31a0242e807ea893b0456b592214c715f07998
Alleph <allephn@hotmail.com.br>
2024-10-31 11:53:06 -0300

    Calculadora de IMC completo

--- Projetos/CalculadoraIMC/app/src/main/java/com/allephnogueira/calculadoraimc/MainActivity.kt
+++ Projetos/CalculadoraIMC/app/src/main/java/com/allephnogueira/calculadoraimc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.allephnogueira.calculadoraimc
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalcular : Button
+    private lateinit var editPeso : EditText // Aqui estamos usando uma caixa de texto.
+    private lateinit var editAltura : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +25,48 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        /**
-         * Abrindo uma nova activity
-         */
+
         btnCalcular = findViewById(R.id.btnCalcular)
+        editPeso = findViewById(R.id.edit_peso)
+        editAltura = findViewById(R.id.edit_altura)
+
+
         btnCalcular.setOnClickListener {
+
             val intent = Intent(this, ResultadoActivity::class.java)
+
+            /**
+             * Quando o usuario clicar no botao vamos
+             *
+             * Recuperar  o texto digitado pelo usuario
+             */
+            val peso = editPeso.text.toString()
+            val altura = editAltura.text.toString()
+
+            /**
+             * Agora imagina se o usuario nao digitar o peso e altura?
+             * isNotEmpty = nao esta vazio.
+             *
+             */
+
+            if (peso.isNotEmpty() && altura.isNotEmpty()) {
+
+                /**
+                 * Lembrar que o peso vem como string, mas queremos ele como Double para fazer o calculo
+                 * Então estamos enviando ele como Double.
+                 * Vamos enviar apenas se o usuario digitar os dados.
+                 */
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
+
+            /**
+             * Abrir uma nova Activity
+             */
+
             startActivity(intent)
         }
 
-        /**
-         * Pegando os itens que o o usuario vai digitar
-         */
 
 
     }
--- Projetos/CalculadoraIMC/app/src/main/java/com/allephnogueira/calculadoraimc/ResultadoActivity.kt
+++ Projetos/CalculadoraIMC/app/src/main/java/com/allephnogueira/calculadoraimc/ResultadoActivity.kt
@@ -1,12 +1,22 @@
 package com.allephnogueira.calculadoraimc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultadoActivity : AppCompatActivity() {
+    /**
+     * Agora aqui vamos capturar esses valores que vinheram da outra activity e fazer o calculo
+     */
+
+    private lateinit var textPeso : TextView
+    private lateinit var textAltura : TextView
+    private lateinit var textResultado : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,48 @@ class ResultadoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById(R.id.text_altura)
+        textResultado = findViewById(R.id.text_resultado)
+
+        /**
+         * Aqui vamos recuperar os valores que vem da outra activity
+         */
+
+        val bundle = intent.extras
+        if (bundle != null){
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            /**
+             * Agora vamos exibir dentro das textView
+             */
+            textPeso.text = ("Peso informado: $peso kg")
+            textAltura.text = ("Altura informada: $altura m")
+
+
+            /**
+             * Formula do calculo de IMC
+             */
+            val imc = peso / (altura * altura)
+
+            /**
+             * Aqui vamos adicionar dentro da variavel o resultado o teste
+             * Repara que colocamos o if diretamente dentro da variavel, para evitar de ficar duplicando codigo.
+             */
+
+            textResultado.text = if(imc < 18.5 ) {
+                "Seu IMC é: Baixo"
+            }else if (imc in 18.5..24.9 ) {
+                "Seu IMC é: Normal"
+            }else if(imc in 25.0..29.9) {
+                "Seu IMC é: Sobrepeso"
+            }else {
+                "Seu IMC é: Obeso"
+            }
+
+        }
+
     }
 }
