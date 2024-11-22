53912181a7db9d589fc7ce6d70dcbf3def196f72
Alleph <allephn@hotmail.com.br>
2024-11-22 10:25:06 -0300

    Introdução a collections

---
+++ Curso-Android-Kotlin/Kotlin/Kotlin Sintaxe/src/Collections/Collections.kt
@@ -0,0 +1,22 @@
+package Collections
+
+/**
+ * São usadas para armazenar lista de listas.
+ *
+ * Principais tipos de lista: List, Set, Map = Imutaveis (Não pode ser modificada)
+ * * IMUTAVEL: Apos ser criada, nao podemos adicionar novos itens, nao podemos remover itens ou seja não podemos mexer no tamanho dela em geral.
+ *
+ * List = Uma lista parecida com Array
+ * Set = Uma lista de itens unicos
+ * Map = Map funciona como uma lista mas eu mesmo crio meus indices.
+ *
+ * * Transformando em mutavel
+ * MutableList - MutableSet - MutableMap
+ * Dessa forma podemos atualizar a lista.
+ *
+ * Metodos dentro do imutavel (List)
+ * [] get, plus, size, indexOF, First, Last, Contains, Shuffled.
+ *
+ * Metodos dentro do MUTAVEL(Posso atualizar)
+ * Todos os metodos antigos + add, Remove, Clear.
+ */
